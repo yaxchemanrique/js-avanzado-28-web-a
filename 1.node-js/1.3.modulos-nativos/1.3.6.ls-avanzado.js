fdbb08974060f85e8aa686c79629681e80746d5f
Yaxche <yaxchemanrique@gmail.com>
2023-10-18 19:50:24 -0700

    fet: callback hell

--- 1.node-js/1.3.modulos-nativos/1.3.6.ls-avanzado.js
+++ 1.node-js/1.3.modulos-nativos/1.3.6.ls-avanzado.js
@@ -2,25 +2,27 @@ const fs = require('node:fs');
 const path = require('node:path');
 const folder = process.argv[2] ?? '.';
 
-fs.readdir(folder, (err, files) => {
-    if(err) {
-        console.log(`No se pudo leer el directiorio ${folder}`);
-        process.exit(1);
-    }
-    files.forEach(file => {
-        const filePath = path.join(folder, file);
-        // console.log(filePath);
-        fs.stat(filePath, (err, stat)=> {
-            if(err) {
-                console.log('Hubo un error al leer el archivo/directorio ', filePath);
-                process.exit(1);
-            }
-            const isDirectory = stat.isDirectory();
-            const fileType = isDirectory ? 'f' : 'a';
-            const fileSize = stat.size.toString();
-            const modifiedDate = stat.mtime.toLocaleString();
+fs.readdir(folder, 
+    (err, files) => {
+        if(err) {
+            console.log(`No se pudo leer el directiorio ${folder}`);
+            process.exit(1);
+        }
+        files.forEach(file => {
+            const filePath = path.join(folder, file);
+            // console.log(filePath);
+            fs.stat(filePath, 
+                (err, stat)=> {
+                    if(err) {
+                        console.log('Hubo un error al leer el archivo/directorio ', filePath);
+                        process.exit(1);
+                    }
+                    const isDirectory = stat.isDirectory();
+                    const fileType = isDirectory ? 'f' : 'a';
+                    const fileSize = stat.size.toString();
+                    const modifiedDate = stat.mtime.toLocaleString();
 
-            console.log(`${fileType} ${filePath.padEnd(30)} ${fileSize.padStart(10)} ${modifiedDate}`);
-        })
-    });
+                    console.log(`${fileType} ${filePath.padEnd(30)} ${fileSize.padStart(10)} ${modifiedDate}`);
+            })
+        });
 })
