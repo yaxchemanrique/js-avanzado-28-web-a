3315f5e7b285268b5df1239095e487ed76384673
Yaxche <yaxchemanrique@gmail.com>
2023-10-18 19:44:37 -0700

    feat: codigo aplicación ls

--- 1.node-js/1.3.modulos-nativos/1.3.6.ls-avanzado.js
+++ 1.node-js/1.3.modulos-nativos/1.3.6.ls-avanzado.js
@@ -9,10 +9,18 @@ fs.readdir(folder, (err, files) => {
     }
     files.forEach(file => {
         const filePath = path.join(folder, file);
-        console.log(filePath);
-        //archivo o folder?
-        // nombre               //fs.stat
-        //fileSize
-        //fecha de modificacion
+        // console.log(filePath);
+        fs.stat(filePath, (err, stat)=> {
+            if(err) {
+                console.log('Hubo un error al leer el archivo/directorio ', filePath);
+                process.exit(1);
+            }
+            const isDirectory = stat.isDirectory();
+            const fileType = isDirectory ? 'f' : 'a';
+            const fileSize = stat.size.toString();
+            const modifiedDate = stat.mtime.toLocaleString();
+
+            console.log(`${fileType} ${filePath.padEnd(30)} ${fileSize.padStart(10)} ${modifiedDate}`);
+        })
     });
 })
