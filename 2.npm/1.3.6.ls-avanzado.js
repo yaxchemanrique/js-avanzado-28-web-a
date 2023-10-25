795a7e5522485a78e5507354a420dd40ee1cd16b
Yaxche Manrique <yaxchemanrique@gmail.com>
2023-10-25 16:58:41 -0700

    feat: routing

---
+++ 2.npm/1.3.6.ls-avanzado.js
@@ -0,0 +1,29 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const pc = require('picocolors');
+const folder = process.argv[2] ?? '.';
+
+fs.readdir(folder, 
+    (err, files) => {
+        if(err) {
+            console.log(pc.magenta(`⚠️ No se pudo leer el directiorio ${folder}`));
+            process.exit(1);
+        }
+        files.forEach(file => {
+            const filePath = path.join(folder, file);
+            // console.log(filePath);
+            fs.stat(filePath, 
+                (err, stat)=> {
+                    if(err) {
+                        console.log('Hubo un error al leer el archivo/directorio ', filePath);
+                        process.exit(1);
+                    }
+                    const isDirectory = stat.isDirectory();
+                    const fileType = isDirectory ? 'f' : 'a';
+                    const fileSize = stat.size.toString();
+                    const modifiedDate = stat.mtime.toLocaleString();
+
+                    console.log(`${pc.green(fileType)} ${pc.blue(filePath.padEnd(30))} ${fileSize.padStart(10)} ${modifiedDate}`);
+            })
+        });
+})
