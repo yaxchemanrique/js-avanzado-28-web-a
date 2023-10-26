4d9acd3e2244f0474338763e11557a4ae909759e
Yaxche Manrique <yaxchemanrique@gmail.com>
2023-10-25 18:44:18 -0700

    feat: rutas y vistas

--- 3.express/routes/users.js
+++ 3.express/routes/users.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
+// /users/
+// /users/new
+// /users/3
+// /users/:id
 
-
+// users/
 router.get('/', (req, res) => {
     res.send('lista de usuarios');
 })
 
+// users/new
 router.get('/new', (req, res) => {
-    res.send('formulario para crear nuevo usuario');
+    // res.send('formulario para crear nuevo usuario');
+    // todos los render se van a ir a buscar a /views
+    res.render('users/new');
+})
+
+
+// forma de enviar informacion: JSON 
+// users/
+router.post('/', (req, res) => {
+    // console.log(res);
+    console.log(req.body.firstName);
+    res.send('res.send de users.js');
 })
 
 // /users/1
+// /users/:id
 router.get('/:id', (req, res) => {
     const userId = req.params.id;
     console.log(req.params);
--- 3.express/server.js
+++ 3.express/server.js
@@ -2,15 +2,25 @@ const express = require('express');
 const port = 3000;
 
 const app = express();
-// 'pug'
+app.use(express.urlencoded({ extended: true }))
+// 'pug, hbs'
 app.set('view engine', 'ejs');
 
+// Para usar una pagina que requiere contenido dinamico
 app.get('/', (req, res) => {
     // pra probar, generico
     // res.send('Hola Mundo!');
     res.render('index', { text: 'este es texto desde server.js'});
-})
+}) 
+
+// app.get('/contacto')
+// app.get('/mi-perfil')
+
+
 /* 
+Esto es mucha repeticion de /users/
+Nuestro codigo debe de ser DRY
+
 app.get('/users', (req, res) => {
     res.send('lista de usuarios');
 })
@@ -19,6 +29,9 @@ app.get('/users/new', (req, res) => {
     res.send('formulario para crear nuevo usuario');
 })
  */
+
+// Para usar una pagina Estática
+// app.use(express.static('public'));
 
 const usersRouter = require('./routes/users.js');
 app.use('/users', usersRouter);
